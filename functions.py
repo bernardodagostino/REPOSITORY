a860afa71c77de290a21583b1c7d1a352362fc50
Bernardo D'Agostino <b.dagostino2@studenti.unipi.it>
2023-10-24 14:14:16 -0400

    modifiche finali alla creazione delle prime tavole

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import csv
+import json
+
 def check_if_in_list(l_item, l_list):
     i = 0
     for i in range(len(l_list)):
@@ -15,7 +18,39 @@ def find_ind(col_names, col_of_int):
     return ind
 
 
-def add_unique_ids(data, colums, id_name='id'):
+def write_to_csv(file, path):
+    with open(path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(file)
+
+
+def add_crime_gravity(data, age_path="DATA//dict_partecipant_age.json", status_path="DATA//dict_partecipant_status.json", type_path="DATA//dict_partecipant_type.json"):
+    # load json file content as a dictionary
+    with open(age_path) as f:
+        dic_p_age = json.load(f)
+
+    with open(status_path) as f:
+        dic_p_status = json.load(f)
+
+    with open(type_path) as f:
+        dic_p_type = json.load(f)
+
+    col_of_int = ['participant_age_group', 'participant_status', 'participant_type']
+    col_id = find_ind(data[0], col_of_int)
+
+    crime_gravity = []
+
+    data[0].append('crime_gravity')
+
+    # for each row of the dataset minus the header
+    for obs in data[1:]:
+        obs.append(dic_p_age[obs[col_id[0]]] * dic_p_status[obs[col_id[1]]] * dic_p_type[obs[col_id[2]]])
+
+    return data
+
+
+
+def add_unique_ids(data, colums, id_name='id', path = 'DATA//NEW_TAB//NEW_TAB.csv'):
     new_table = []
     id_count = 0
 
@@ -44,11 +79,14 @@ def add_unique_ids(data, colums, id_name='id'):
         for i in sorted(col_in, reverse=True):
             row.pop(i)
 
+    
+    write_to_csv(new_table, path)
+
 
     return data, new_table
 
 
-def add_unique_ids_2(data, columns, id_name='id'):
+def add_unique_ids_2(data, columns, id_name='id', path = 'DATA//NEW_TAB//NEW_TAB.csv'):
     new_table = []
     id_count = 0
 
@@ -73,5 +111,7 @@ def add_unique_ids_2(data, columns, id_name='id'):
 
         for i in sorted(col_in, reverse=True):
             row.pop(i)
+
+    write_to_csv(new_table, path)
     
     return data, new_table
